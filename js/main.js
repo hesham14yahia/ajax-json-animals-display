77cc17db82e64d5a3b6f8054b5dc3446735eeeec
Hehsam Yahia <hesham14yahia@gmail.com>
2018-07-31 13:17:47 +0200

    filling json data

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function (){
     var ourRequest = new XMLHttpRequest;
-    ourRequest.open("GET", "https://learnwebcode.github.io/json-example/animals-"+ urlCount +".json");
+    ourRequest.open("GET", "res/animal-"+ urlCount +".json");
     ourRequest.onload = function() {
         var ourData = JSON.parse(ourRequest.responseText);
         disHtml(ourData);
